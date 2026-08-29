41e955d4cecf80239c974242c4acc97c9c91cc11
ali_balochi <alibalochi1910@gmail.com>
2026-08-29 14:01:06 +0330

    optimizing create book form

--- book/form.py
+++ book/form.py
@@ -1,25 +1,78 @@
 from django.forms.models import ModelForm
 from book.models import Book, Category
+from django import forms
 
 
 class CrateBookForm(ModelForm):
+
     class Meta:
         model = Book
-        fields = ["title", "price", "pages", "author", "pub_date", "category"]
+
+        fields = [
+            "title",
+            "price",
+            "pages",
+            "author",
+            "pub_date",
+            "category",
+        ]
+
+        widgets = {
+            "pub_date": forms.DateInput(
+                attrs={
+                    "type": "date",
+                    "class": "w-full rounded-2xl border border-black/10 bg-white px-5 py-4 text-sm font-medium outline-none transition focus:border-black focus:ring-4 focus:ring-black/5",
+                }
+            ),
+        }
 
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
 
-        for field in self.fields.values():
-            field.widget.attrs.update(
-                {
-                    "class": "form-control",
-                }
-            )
+        input_class = (
+            "w-full "
+            "rounded-2xl "
+            "border border-black/10 "
+            "bg-white "
+            "px-5 py-4 "
+            "text-sm font-medium "
+            "outline-none "
+            "transition "
+            "placeholder:text-black/30 "
+            "hover:border-black/20 "
+            "focus:border-black "
+            "focus:ring-4 "
+            "focus:ring-black/5"
+        )
+
+        select_class = (
+            "w-full "
+            "rounded-2xl "
+            "border border-black/10 "
+            "bg-white "
+            "px-5 py-4 "
+            "text-sm font-medium "
+            "outline-none "
+            "transition "
+            "hover:border-black/20 "
+            "focus:border-black "
+            "focus:ring-4 "
+            "focus:ring-black/5"
+        )
+
+        for name, field in self.fields.items():
+
+            if name != "pub_date":
+                field.widget.attrs.update(
+                    {
+                        "class": input_class,
+                    }
+                )
 
         self.fields["category"].widget.attrs.update(
             {
-                "class": "form-select",
+                "class": select_class,
             }
         )
 
